d1cd9a3c8f1e964871f0a00d8418725ab84c395b
hkhuran <hkhuran@ilstu.edu>
2016-02-08 19:18:21 -0600

    1st undate in customer_view.php file
    Php code to establish connection to the database was added to the
    existing file that already contains html code.

--- customer_view.php
+++ customer_view.php
@@ -26,7 +26,58 @@
                 </div>
             </div>
         </nav>
-        
+        <!-- Start of php script -->
+        <?php
+        // Database credentials 
+        $hn = 'www.it354.com';
+        $db = 'it354_students';
+        $un = 'it354_students';
+        $pw = 'steinway';
+
+        // To connect to database
+        $conn = new mysqli($hn, $un, $pw, $db);
+        if ($conn->connect_error)
+            die($conn->connect_error);
+
+        // Query to get the data from the database
+        $query = "SELECT * FROM customers";
+        // Execution of the query
+        $result = $conn->query($query);
+        if (!$result)
+        // cecking for database connection error
+            die($conn->error);
+        $rows = $result->num_rows;
+
+        // Creating a container for table and echo it and general layout for the table.
+        echo '<div class="container">';
+        echo '<table class="table table-hover">';
+        echo '<thead>';
+        echo '<tr>';
+        echo '<th>ID</th>';
+        echo '<th>Last/Company Name</th>';
+        echo '<th>First Name</th>';
+        echo '<th>Address</th>';
+        echo '<th>City</th>';
+        echo '<th>State</th>';
+        echo '<th>Zip</th>';
+        echo '<th>Email</th>';
+        echo '<th>Phone</th>';
+        echo'</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+
+        }
+        // Closing the table layout.
+        echo'</tbody>';
+        echo'</table>';
+        echo'</div>';
+
+        // Closing the result from the query
+        $result->close();
+
+        // Closing the database connection
+        $conn->close();
+        ?>
         <!-- Closing the body and html -->
     </body>
 </html>
