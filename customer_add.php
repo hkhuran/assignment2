0062e0b1ed18f0efcec2aa5b2d5c62134a4f3b34
hkhuran <hkhuran@ilstu.edu>
2016-02-08 19:39:20 -0600

    Changes in Partner B file.
    Made changes to add a sql file and added code to print the success and
    failure of the database connection.

--- customer_add.php
+++ customer_add.php
@@ -11,13 +11,19 @@ $conn = new mysqli($hn, $un, $pw, $db);
 if ($conn->connect_error)
     die($conn->connect_error);
 
-
+// Query to delete the data
+// $query = "Delete from customers where firstName = 'Aaron'";
 // Query to insert the user data into the database
 $query = "INSERT INTO customers (firstName, lastName, address, city, state, zip, email, phone) "
         . "VALUES('$_POST[FName]', '$_POST[LCompany]', '$_POST[address]', '$_POST[city]', '$_POST[state]', '$_POST[zip]', '$_POST[email]', '$_POST[phone]')";
 
 
-
+// Printing the result of the query
+if ($conn->query($query) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $query . "<br>" . $conn->error;
+}
 
 // Closing the database
 $conn->close();
