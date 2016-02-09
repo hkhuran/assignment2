05a8a0de1df7991d6180fc853d0d708c6a392093
hkhuran <hkhuran@ilstu.edu>
2016-02-08 19:09:50 -0600

    Customer_view.php
    This file is the general layout of the page. The file contains the html
    code.

---
+++ customer_view.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title>Assignment 2</title>
+        <meta name="description" content="">
+        <!-- Mobile viewport optimized -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <!-- Bootstrap CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <!-- Start of the Navigation bar -->
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Heading in the Navigation bar -->
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Assignment 2</a>
+                </div>
+
+                <!-- View and Add option in the Navigation bar -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="customer_view.php">View</a></li>
+                        <li><a href="customer_add.html">Add</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <!-- Closing the body and html -->
+    </body>
+</html>
