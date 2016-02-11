2c21c89c16f0d4d1ac3a105db9763478f35925ed
Rajneesh Kumar Pandey <rajneesh2017@gmail.com>
2016-02-11 05:41:51 -0600

    Added nav bar to customer_add.php
    Changes have been made to the customer_add.php file by adding the nav
    bar code.

--- customer_add.php
+++ customer_add.php
@@ -1,3 +1,32 @@
+<html>
+    <head>
+        <title>Assignment 2</title>
+        <meta name="description" content="">
+        <!-- Mobile viewport optimized -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <!-- Bootstrap CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <!-- Start of the Navigation bar -->
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Heading in the Navigation bar -->
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Assignment 2</a>
+                </div>
+
+                <!-- View and Add option in the Navigation bar -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="customer_view.php">View</a></li>
+                        <li><a href="customer_add.html">Add</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Start of php script -->
 <?php
 
 // Database credentials 
@@ -20,7 +49,7 @@ $query = "INSERT INTO customers (firstName, lastName, address, city, state, zip,
 
 // Printing the result of the query
 if ($conn->query($query) === TRUE) {
-    echo "New record created successfully";
+	echo "New record created successfully";
 } else {
     echo "Error: " . $query . "<br>" . $conn->error;
 }
@@ -28,3 +57,6 @@ if ($conn->query($query) === TRUE) {
 // Closing the database
 $conn->close();
 ?>
+  <!-- Closing the body and html -->
+    </body>
+</html>
